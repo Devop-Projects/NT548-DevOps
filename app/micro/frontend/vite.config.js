9da03c374cdc40606529c63911bbaf9a27117822
vantai13 <taidoan.13072005@gmail.com>
2026-04-25 13:06:55 +0700

    complete app

--- app/micro/frontend/vite.config.js
+++ app/micro/frontend/vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Bắt buộc khi chạy trong Docker
+    allowedHosts: ['frontend', 'localhost'] // Cho phép tên miền nội bộ của Nginx đi qua
+  }
 })
